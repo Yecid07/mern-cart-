2bbee900414dc94d6e94e046e1d63b4a5f710576
Yecid Perea <yecidperea9@gmail.com>
2025-08-12 23:47:43 -0500

    App.jsx correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import { Box , useColorModeValue} from '@chakra-ui/react';
 import { Route, Routes } from 'react-router-dom';
 import CreatePage from './pages/CreatePage';
-import  HomePage from './pages/Homepage';
+import HomePage from './pages/Homepage';
 import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <Box minH={"100vh"} bg={useColorModeValue("gray.100","gray.900")}>
-      <Navbar/> 
+      <Navbar /> 
       <Routes>
-        <Route path="/" element = {<HomePage />}/>
-        <Route path="/create" element = {<CreatePage />}/>
+        <Route path="/" element = {<HomePage />} />
+        <Route path="/create" element = {<CreatePage />} />
       </Routes>
     </Box>
   );
